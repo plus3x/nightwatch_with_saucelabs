5976e3f3840fa2d6762b9f53f617a33fcd635d6a
Vladislav Petrov <electronicchest@gmail.com>
2017-03-21 04:39:01 +0300

    Wait body

--- tests/demo_test.js
+++ tests/demo_test.js
@@ -2,7 +2,7 @@ module.exports = {
   'Demo test' : function (browser) {
     browser
       .url(browser.launchUrl)
-      .waitForElementVisible('body')
+      .waitForElementVisible('body', 1000)
       .end();
   }
 };
