87fbe0cecf957f8d79c6bfdd38a32a74cddbdfbc
Vladislav Petrov <electronicchest@gmail.com>
2017-03-22 20:42:33 +0300

    Add some environments

--- nightwatch.saucelabs.js
+++ nightwatch.saucelabs.js
@@ -1,3 +1,13 @@
+function desired_capabilities(platform, browser, version) {
+  return {
+    desiredCapabilities: {
+      platform: platform,
+      browserName: browser,
+      version: version
+    }
+  }
+}
+
 module.exports = {
   src_folders: ['tests'],
   output_folder: 'reports',
@@ -26,16 +36,30 @@ module.exports = {
       }
     },
 
-    chrome: {
-      desiredCapabilities: {
-        browserName: 'chrome'
-      }
-    },
+    macos_10_12_chrome_56_0:  desired_capabilities('macOS 10.12',  'chrome', '56.0'),
+    macos_10_12_chrome_55_0:  desired_capabilities('macOS 10.12',  'chrome', '55.0'),
+    macos_10_12_firefox_51_0: desired_capabilities('macOS 10.12', 'firefox', '51.0'),
+    macos_10_12_firefox_50_0: desired_capabilities('macOS 10.12', 'firefox', '50.0'),
+    macos_10_12_safari_10_0:  desired_capabilities('macOS 10.12',  'safari', '10.0'),
+    macos_10_11_chrome_56_0:  desired_capabilities('macOS 10.11',  'chrome', '56.0'),
+    macos_10_11_chrome_55_0:  desired_capabilities('macOS 10.11',  'chrome', '55.0'),
+    macos_10_11_firefox_51_0: desired_capabilities('macOS 10.11', 'firefox', '51.0'),
+    macos_10_11_firefox_50_0: desired_capabilities('macOS 10.11', 'firefox', '50.0'),
+    macos_10_11_safari_10_0:  desired_capabilities('macOS 10.11',  'safari', '10.0'),
 
-    firefox: {
-      desiredCapabilities: {
-        browserName: 'firefox'
-      }
-    }
+    win_10_chrome_56_0:       desired_capabilities( 'Windows 10',  'chrome', '56.0'),
+    win_10_chrome_55_0:       desired_capabilities( 'Windows 10',  'chrome', '55.0'),
+    win_10_firefox_51_0:      desired_capabilities( 'Windows 10', 'firefox', '51.0'),
+    win_10_firefox_50_0:      desired_capabilities( 'Windows 10', 'firefox', '50.0'),
+    win_10_ie_11_103:         desired_capabilities( 'Windows 10', 'internet explorer', '11.103'),
+    win_10_edge_14_14393:     desired_capabilities( 'Windows 10',    'edge', '14.14393'),
+    win_10_edge_13_10586:     desired_capabilities( 'Windows 10',    'edge', '13.10586'),
+    win_8_1_chrome_56_0:      desired_capabilities( 'Windows 8.1',  'chrome', '56.0'),
+    win_8_1_chrome_55_0:      desired_capabilities( 'Windows 8.1',  'chrome', '55.0'),
+    win_8_1_firefox_51_0:     desired_capabilities( 'Windows 8.1', 'firefox', '51.0'),
+    win_8_1_firefox_50_0:     desired_capabilities( 'Windows 8.1', 'firefox', '50.0'),
+    win_8_1_ie_11_103:        desired_capabilities( 'Windows 8.1', 'internet explorer', '11.103'),
+    win_8_1_edge_14_14393:    desired_capabilities( 'Windows 8.1',    'edge', '14.14393'),
+    win_8_1_edge_13_10586:    desired_capabilities( 'Windows 8.1',    'edge', '13.10586')
   }
-};
+}
